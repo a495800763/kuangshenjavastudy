b02333f9f20d03ab078daa27bf31edd0cbad7b9d
a495800763 <495800763@qq.com>
2022-05-29 18:32:57 +0800

    InetAddress 类

---
+++ src/main/java/com/liumengqi/networkprograming/Test.java
@@ -0,0 +1,34 @@
+package com.liumengqi.networkprograming;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 测试IP
+ */
+public class Test {
+    public static void main(String[] args) {
+        try {
+            InetAddress inetAddress1 = InetAddress.getByName("127.0.0.1");
+            System.out.println(inetAddress1);
+            InetAddress inetAddress3 = InetAddress.getByName("localhost");
+            System.out.println(inetAddress3);
+            InetAddress inetAddress4 = InetAddress.getLocalHost();
+            System.out.println(inetAddress4);
+            //查询网络地址
+            InetAddress inetAddress2 = InetAddress.getByName("www.baidu.com");
+            System.out.println(inetAddress2);
+
+            //常用方法
+            System.out.println(inetAddress2.getAddress());
+            //获取规范的名称
+            System.out.println(inetAddress2.getCanonicalHostName());
+            //获取IP
+            System.out.println(inetAddress2.getHostAddress());
+            //获取域名
+            System.out.println(inetAddress2.getHostName());
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
