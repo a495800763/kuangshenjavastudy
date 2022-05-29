3131ef0d202378d9fe2c0280fc2ec105e4d01c3b
a495800763 <495800763@qq.com>
2022-05-29 21:15:52 +0800

    TCP 协议下使用socket类实现消息发送 循环发送循环接收

--- src/main/java/com/liumengqi/networkprograming/tcpdemo/TcpClint.java
+++ src/main/java/com/liumengqi/networkprograming/tcpdemo/TcpClint.java
@@ -12,35 +12,40 @@ import java.net.UnknownHostException;
 public class TcpClint {
     public static void main(String[] args) {
 
-        Socket serverSocket=null;
-        OutputStream os=null;
-        try {
-            //1 要知道服务器的地址
-            InetAddress serverIP = InetAddress.getByName("127.0.0.1");
-            // 要知道端口号
-            int port = 9999;
-            //创建一个socket链接
-            serverSocket = new Socket(serverIP, port);
-            //使用IO流发送消息
-            os = serverSocket.getOutputStream();
-            os.write("你好，欢迎学习java网络编程相关知识".getBytes());
-        }  catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            if(os!=null){
-                try {
-                    os.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+        Socket serverSocket = null;
+        OutputStream os = null;
+        int index= 0;
+        while (index <100){
+            try {
+                //1 要知道服务器的地址
+                InetAddress serverIP = InetAddress.getByName("127.0.0.1");
+                // 要知道端口号
+                int port = 9999;
+                //创建一个socket链接
+                serverSocket = new Socket(serverIP, port);
+                //使用IO流发送消息
+                os = serverSocket.getOutputStream();
+                os.write(("你好，欢迎学习java网络编程相关知识"+"当前是第【"+(index+1)+"】条消息").getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (os != null) {
+                    try {
+                        os.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-            if(serverSocket!=null){
-                try {
-                    serverSocket.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (serverSocket != null) {
+                    try {
+                        serverSocket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
+                index++;
             }
         }
+
     }
 }
