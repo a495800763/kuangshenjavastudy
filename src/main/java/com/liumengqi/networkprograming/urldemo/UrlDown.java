f710a26c0dc49124e0ae8045b34b61c4a4737b6b
a495800763 <495800763@qq.com>
2022-05-31 22:11:10 +0800

    URL  使用数据流下载文件

---
+++ src/main/java/com/liumengqi/networkprograming/urldemo/UrlDown.java
@@ -0,0 +1,32 @@
+package com.liumengqi.networkprograming.urldemo;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class UrlDown {
+
+    private static final String URL = "http://localhost:8080/liumengqi/exampleFile.txt";
+
+    public static void main(String[] args) throws Exception {
+        URL url = new URL(URL);
+
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+        InputStream inputStream = connection.getInputStream();
+
+        FileOutputStream fos = new FileOutputStream("downloadFile.txt");
+
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len=inputStream.read(buffer))!=-1){
+            fos.write(buffer,0,len);
+        }
+
+        fos.close();
+        inputStream.close();
+        //断开连接
+        connection.disconnect();
+    }
+}
